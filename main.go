3ed498af5b9daf455eb79d8e89fe2118d6b8a954
Christopher Danz <115592722+christopher-danz@users.noreply.github.com>
2022-12-12 11:25:38 +0100

    create main.go

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/nlopes/slack"
+)
+
+func main() {
+	token := os.Getenv("SLACK_BOT_TOKEN")
+	api := slack.New(token)
+
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+	for msg := range rtm.IncomingEvents {
+		switch ev := msg.Data.(type) {
+		case *slack.HelloEvent:
+			// Ignore hello
+
+		case *slack.ConnectedEvent:
+			fmt.Println("Infos:", ev.Info)
+			fmt.Println("Connection counter:", ev.ConnectionCount)
+
+		case *slack.MessageEvent:
+			fmt.Printf("Message: %v\n", ev)
+			rtm.SendMessage(rtm.NewOutgoingMessage("Hello world!", ev.Channel))
+
+		case *slack.PresenceChangeEvent:
+			fmt.Printf("Presence Change: %v\n", ev)
+
+		case *slack.LatencyReport:
+			fmt.Printf("Current latency: %v\n", ev.Value)
+
+		case *slack.RTMError:
+			fmt.Printf("Error: %s\n", ev.Error())
+
+		case *slack.InvalidAuthEvent:
+			fmt.Printf("Invalid credentials")
+			return
+
+		default:
+
+			// Ignore other events..
+			// fmt.Printf("Unexpected: %v\n", msg.Data)
+		}
+	}
+}
